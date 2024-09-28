a23b95345d55256a844267b509932f79def01be0
HectorCRZBQ <148070442+HectorCRZBQ@users.noreply.github.com>
2024-09-28 02:45:12 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function changeCharacterName() {
-    // Captura el valor del input
-    const characterName = document.getElementById('characterNameInput').value;
-
-    // Asigna el nombre ingresado al elemento que muestra el nombre del personaje
-    document.getElementById('characterNameDisplay').textContent = characterName;
-}
